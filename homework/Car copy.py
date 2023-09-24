0379debd65612a0e1f4533928f6abd28f1b1863c
xoth42 <xoth42@protonmail.com>
2023-09-24 17:46:54 -0400

    suffering and agony

--- homework/Car copy.py
+++ homework/Car copy.py
@@ -16,4 +16,15 @@ class Car:
         self.max_passengers=num 
     def get_max_passengers(self):
         return self.max_passengers
-
+        
+    def __str__(self):
+        toReturn = "Make and Model: " + car1.get_make_model() + "\nNumber of doors: " +str(car1.get_num_doors()) + "\nMaximum number of passengers: "+str(car1.get_max_passengers())
+        return toReturn
+    
+if __name__=="__main__":
+    car1 = Car()
+    car1.set_make_model("Dodge Dart")
+    car1.set_num_doors(5)
+    car1.set_max_passengers(7)
+    # print("Model", car1.get_make_model(),"doors",car1.get_num_doors(),"max passengers",car1.get_max_passengers())
+    print(car1)
