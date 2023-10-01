65503ca7c1011c9f5dcb2aeff0013fbf953376f2
xoth42 <xoth42@protonmail.com>
2023-10-01 15:19:45 -0400

    yay

---
+++ Lab/Lab3_Inheritance/inheritance.py
@@ -0,0 +1,5 @@
+# LAB WORK FOR SEPT 29 (missed)
+#links:
+# lab sheet https://docs.google.com/document/d/1akctSxxlE4BNSIbJ8hi4Du0GVzJv7I2w/edit
+# gradescope https://www.gradescope.com/courses/560274
+
