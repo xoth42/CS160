ff641c27aa87137561c8a92293764bab989c1c75
xoth42 <xoth42@protonmail.com>
2023-10-31 09:46:07 -0400

    recutseetion

---
+++ October/recursion_time.py
@@ -0,0 +1,16 @@
+# recursion is the best
+# recutsion must have base cases?
+def factorial(n):
+    if (n==0):
+        # base case
+        return 1
+    else:
+        return n*factorial(n-1)
+    
+def fibonacci(n):
+    # ??!?!?
+    return
+if __name__=="__main__":
+    print(factorial(3))
+    
+    
