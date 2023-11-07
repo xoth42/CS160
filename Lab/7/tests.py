ef2745283fe517c94fd475dfdda68e2410102e33
xoth42 <xoth42@protonmail.com>
2023-11-06 20:47:25 -0500

    what?

--- Lab/7/tests.py
+++ Lab/7/tests.py
@@ -8,7 +8,8 @@ class tests(unittest.TestCase):
                         ([1,3,5],[2],       [1,2,3,5]),
                         ([1,2,3],[4,5,6],   [1,2,3,4,5,6]),
                         ([4,4],[4,4],       [4,4,4,4]),
-                        ([-1],[0],[-1,0]))
+                        ([-1],[0],          [-1,0]),
+                        ([-1,0,4],[-0.5,5], [-1,-0.5,0,4,5]))
         for i in range(len(inputsOutputs)):
             self.assertEqual(inputsOutputs[i][2],MergeSort.merge(inputsOutputs[i][0],inputsOutputs[i][1]))
 if __name__=="__main__":
