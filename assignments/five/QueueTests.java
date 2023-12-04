0e4663ec34936d27caba4a08c94abcf9ca6411f2
xoth42 <xoth42@protonmail.com>
2023-12-04 12:44:01 -0500

    not submited

--- assignments/five/QueueTests.java
+++ assignments/five/QueueTests.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 public class QueueTests{
     @Test
     public void testQueue(){
@@ -14,5 +16,17 @@ public class QueueTests{
         assertEquals((Integer)14, q.peek());
         q.dequeue();
         assertEquals(null, q.peek());
+        assertEquals(null, q.dequeue());
+        q.enqueue(10);
+        q.enqueue(11);
+        q.enqueue(12);
+        q.enqueue(13);
+        assertEquals((Integer)10, q.dequeue());
+        assertFalse(q.isEmpty());
+        assertEquals((Integer)11, q.peek());
+        assertEquals((Integer)11, q.dequeue());
+        assertEquals((Integer)12, q.dequeue());
+        assertEquals((Integer)13, q.dequeue());
+        assertTrue(q.isEmpty());
     }
 }
